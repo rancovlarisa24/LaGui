7a2455d0cf5910e5752d512443e4c1e978dc8fad
larisa249 <larisarancov6@gmail.com>
2025-07-22 15:26:20 +0300

    home

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <header className="nav">
+            <h1 className="nav__logo">La Gui</h1>
+
+            <nav className="nav__links">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+    );
+}
